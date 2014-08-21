7f96f0d2b3ca64b5b4708559db966261020993ab
Tanguy de Courson <tanguy@0x7a69.com>
2014-08-21 15:39:07 -0700

    ok script_name doesnt work for the uri so swithc back to uri and strip off query string

--- library/ngfw/Uri.php
+++ library/ngfw/Uri.php
@@ -90,7 +90,8 @@ class Uri {
      * @return void
      */
     public function __construct() {
-        $this->requestedPath = $_SERVER['SCRIPT_NAME'];
+        $this->requestedPath = $_SERVER['REQUEST_URI'];
+	$this->requestPath = (strstr($this->requestPath, ',') ? substr($this->requestPath, 0, strpos($this->requestPath, ',')) : $this->requestPath);
 	$this->query_string = $_GET;
         if(defined('PUBLIC_PATH')):
             $this->rootPath = PUBLIC_PATH;
